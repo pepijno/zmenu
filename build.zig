13bb74729d3cef591692936140c3bf7e5c52360b
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2022-04-12 23:14:28 +0200

    Add basic command line parameters

--- build.zig
+++ build.zig
@@ -1,28 +1,22 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("dmenu-zig", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
-    exe.linkLibC();
-    exe.linkSystemLibrary("c");
-    exe.linkSystemLibrary("X11");
-    exe.linkSystemLibrary("Xinerama");
-    exe.linkSystemLibrary("Xft");
-    exe.linkSystemLibrary("fontconfig");
+    const zmenu = b.addExecutable("zmenu", "src/main.zig");
+    zmenu.setTarget(target);
+    zmenu.setBuildMode(mode);
+    zmenu.install();
+    zmenu.linkLibC();
+    zmenu.linkSystemLibrary("c");
+    zmenu.linkSystemLibrary("X11");
+    zmenu.linkSystemLibrary("Xinerama");
+    zmenu.linkSystemLibrary("Xft");
+    zmenu.linkSystemLibrary("fontconfig");
 
-    const run_cmd = exe.run();
+    const run_cmd = zmenu.run();
     run_cmd.step.dependOn(b.getInstallStep());
 
     const run_step = b.step("run", "Run the app");
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,37 @@
 const std = @import("std");
 const c = @import("c_imports.zig").c;
 
+const stderr = std.io.getStdErr();
+const stdout = std.io.getStdOut().writer();
+
+const ZMENU_VERSION = "0.0.1";
+
+fn usage() !void {
+    _ = try stderr.write("usage: zmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n");
+    _ = try stderr.write("             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n");
+}
+
 pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    _ = args.skip();
+
+    while (args.next(allocator)) |arg| {
+        const argument = try arg;
+        std.debug.print("{s}\n", .{argument});
+
+        if (std.mem.eql(u8, argument, "-v")) {
+            try stdout.print("zmenu-{s}\n", .{ZMENU_VERSION});
+            std.process.exit(0);
+        } else {
+            try usage();
+            std.process.exit(1);
+        }
+    }
+
+    std.process.exit(0);
 }
